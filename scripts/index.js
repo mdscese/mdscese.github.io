b23a51ed9a324b4504e89c9b82441213f9033804
Marcus Scese <mdscese@mtu.edu>
2024-05-06 02:21:18 -0400

    Final UI touches on intial window and begining stages of project display on initial window

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,43 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const imageContainers = document.querySelectorAll(".image-container");
+
+  imageContainers.forEach(imageContainer => {
+    const folderPath = imageContainer.getAttribute("data-folder");
+    fetchImages(folderPath, imageContainer);
+  });
+
+  function fetchImages(folderPath, container) {
+    // Log the folder path
+    console.log("Folder path:", folderPath);
+
+    // Fetch the images from the folder
+    fetch(folderPath)
+      .then(response => response.text())
+      .then(text => {
+        // Parse HTML text to extract image filenames
+        const parser = new DOMParser();
+        const htmlDoc = parser.parseFromString(text, "text/html");
+        const imageFilenames = Array.from(htmlDoc.querySelectorAll("a"))
+          .map(a => a.getAttribute("href"))
+          .filter(href => /\.(jpeg|jpg|gif|png)$/i.test(href));
+
+        // Log the files found at the folder path
+        console.log("Files found:", imageFilenames);
+
+        // Append images to the container
+        imageFilenames.forEach(filename => {
+          const img = document.createElement("img");
+          const imagePath = folderPath.endsWith("/") ? folderPath : folderPath + "/";
+          console.log("imagepath", imagePath);
+          console.log("filename", filename);
+          img.src = filename;
+          img.alt = filename;
+          container.appendChild(img);
+        });
+      })
+      .catch(error => console.error("Error fetching images:", error));
+  }
+
   var currentYear = new Date().getFullYear();
   document.getElementById("currentYear").innerText = currentYear;
   document.getElementById("currentYearSmall").innerText = currentYear;
