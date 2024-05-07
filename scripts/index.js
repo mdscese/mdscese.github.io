f5a62eb0058aacbb44b118ad497633a90e69e7b4
Marcus Scese <mdscese@mtu.edu>
2024-05-07 00:01:22 -0400

    Finished About Me UI

--- scripts/index.js
+++ scripts/index.js
@@ -3,13 +3,17 @@ document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("currentYear").innerText = currentYear;
   document.getElementById("currentYearSmall").innerText = currentYear;
 
-    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
-  
-    scrollToTopBtn.addEventListener("click", function() {
-      // Scroll to the top of the document
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth"
+  var scrollToTopBtns = document.querySelectorAll("#scrollToTopBtn");
+
+  // Loop through each scrollToTopBtn
+  scrollToTopBtns.forEach(function(scrollToTopBtn) {
+      // Attach click event listener to each scrollToTopBtn
+      scrollToTopBtn.addEventListener("click", function() {
+          // Scroll to the top of the document
+          window.scrollTo({
+              top: 0,
+              behavior: "smooth"
+          });
       });
-    });
   });
+});
